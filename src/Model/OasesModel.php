4da83f2d23875a931b79ab5e6cce6202c2663c4e
Kingmaker <b.g.pratheep.cool@gmail.com>
2026-07-02 22:06:51 +0530

    Oasis Animal speed factor

--- src/Model/OasesModel.php
+++ src/Model/OasesModel.php
@@ -35,7 +35,8 @@ class OasesModel
         $units = $units->fetch_assoc();
 
         $gameProgress = (time() - getGame('start_time')) / (getGame('round_length') * 86400);
-        $maximum = 15;
+        $speedFactor = max(1, getGameSpeed() / 50);
+        $maximum = (int)round(15 * $speedFactor);
         $sum = 0;
         for ($i = 1; $i <= 10; ++$i) {
             $sum += $units['u' . $i];
@@ -64,7 +65,8 @@ class OasesModel
         for($i = 1; $i <= $num; ++$i){
             shuffle($troops);
             $unitId = $troops[mt_rand(0, sizeof($troops) - 1)];
-            $amount = mt_rand(1, 3);
+            $amount = (int)round(mt_rand(1, 3) * $speedFactor);
+            $amount = min($amount, $maximum - $sum);
             $db->query("UPDATE units SET u{$unitId}=u{$unitId}+$amount WHERE kid=$kid");
 
             $sum += $amount;
